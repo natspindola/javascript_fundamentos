ebaeabc03c3cc33461ee5e42cb350b5377df4357
natspindola <natspindola@hotmail.com>
2022-03-07 21:30:40 -0300

    fundamentos

---
+++ scripts.js
@@ -0,0 +1,18 @@
+console.log("Hello Word!");
+console.log(123);
+
+var nome = "Guilherme";
+var nome2 = "Joao";
+//var idade = 26;
+//var outravariavel = true;
+var nomeFinal = nome + nome2;
+
+var numl = 10;
+var num2 = 20;
+var resultado = numl + num2;
+
+if (numl < num2) {
+    console.log("verdade");
+} else {
+    console.log("falso");
+}
